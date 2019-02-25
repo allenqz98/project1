d2cccae1d2a70da6e65ab345d0abef5b5b7fc4cb
allenqz98 <qiangzhe@live.cn>
2019-02-25 21:49:06 +0000

    project01

---
+++ start.sh
@@ -0,0 +1,18 @@
+
+rt MIX_ENV=prod
+export PORT=4794
+
+echo "Stopping old copy of app, if any..."
+
+_build/prod/rel/memory/bin/memory stop || true
+
+echo "Starting app..."
+
+# Start to run in background from shell.
+#_build/prod/rel/memory/bin/memory start
+
+# Foreground for testing and for systemd
+_build/prod/rel/memory/bin/memory foreground
+
+# TODO: Add a cron rule or systemd service file
+#       to start your app on system boot.
